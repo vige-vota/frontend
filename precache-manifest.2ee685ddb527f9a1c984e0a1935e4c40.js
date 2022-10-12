9531a442c561661194989e9197980598d39bdbc5
Luca Stancapiano <lucastancapiano@MacBook-Pro.wind3.hub>
2022-10-12 13:04:34 +0200

    Updates

---
+++ precache-manifest.2ee685ddb527f9a1c984e0a1935e4c40.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "aef426f4048b05008312",
+    "url": "/static/css/main.4f78880a.chunk.css"
+  },
+  {
+    "revision": "aef426f4048b05008312",
+    "url": "/static/js/main.aef426f4.chunk.js"
+  },
+  {
+    "revision": "7d2da0afb20e3224f52c",
+    "url": "/static/css/1.1ef24e93.chunk.css"
+  },
+  {
+    "revision": "7d2da0afb20e3224f52c",
+    "url": "/static/js/1.7d2da0af.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5067911c356cc71d6c862622346ca997",
+    "url": "/static/media/x.5067911c.png"
+  },
+  {
+    "revision": "75de78222fe287a4cada1c2465c5abc5",
+    "url": "/static/media/background.75de7822.png"
+  },
+  {
+    "revision": "b29a888ff7f07091c7e08eb0d991e221",
+    "url": "/static/media/primeicons.b29a888f.ttf"
+  },
+  {
+    "revision": "943c3597cd33be56d53df0d1982fa8ff",
+    "url": "/static/media/primeicons.943c3597.woff"
+  },
+  {
+    "revision": "e01fd4133bac49cd2ea07ad6f7c45695",
+    "url": "/static/media/primeicons.e01fd413.eot"
+  },
+  {
+    "revision": "64b5d470af63a67aa9b9d3e8a866a35e",
+    "url": "/static/media/primeicons.64b5d470.svg"
+  },
+  {
+    "revision": "f5ae12ae1ce4e180f938220ed0f57911",
+    "url": "/index.html"
+  }
+];
